3907c494f459afe27a11bc58eedd2bbcd8be0a2b
medvedievigor <medvedev.mmc@gmail.com>
2018-08-05 12:33:48 +0300

    added Load Properties class and properties file

---
+++ src/test/java/properties/LoadProperties.java
@@ -0,0 +1,60 @@
+package properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class LoadProperties {
+    private String userName;
+    private String password;
+    private String url;
+
+    public String getUserName () {
+        return userName;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public String getsUrl () {
+        return url;
+    }
+
+    public String   getProjectProperty() {
+
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+
+        try {
+
+            input = new FileInputStream("jira.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value
+           userName= prop.getProperty("userName");
+           password=prop.getProperty("password");
+           url=prop.getProperty("URL");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+        return url;
+    }
+}
